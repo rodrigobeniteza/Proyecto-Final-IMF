8428ecc9edc0ff0bf6cf85636eac02075952affb
rodrigobenitez <59978290+rodrigobenitez@users.noreply.github.com>
2022-12-05 20:05:51 -0300

    Update Busquedas por user.sql

--- Data/Busquedas por user.sql
+++ Data/Busquedas por user.sql
@@ -5,7 +5,11 @@
 
 SELECT 
     First_Segmentation
-  , search_profile as search_profile
+  , CASE 
+      WHEN (search_profile > 0
+        OR product_clicked_dummy > 0
+        OR product_choice_opened_dummy > 0) THEN 1
+      ELSE 0 END as product_interaction
   , count(DISTINCT user_id)
 FROM `peya-food-and-groceries.user_rodrigo_benitez.qc_users_0_orders_base`
 
@@ -13,12 +17,9 @@ WHERE TRUE
   AND First_Segmentation IN ( 'Y_Food_&_N_QC'
                             , 'N_Food_&_N_QC'
                             ) 
-  AND (
-          product_clicked_dummy = 0
-      OR  product_choice_opened_dummy = 0
-      OR  add_to_cart_dummy = 0
-      OR  cart_clicked_dummy = 0
-      OR  checkout_loaded_dummy = 0
-    )
+  AND add_to_cart_dummy = 0
+  AND cart_clicked_dummy = 0
+  AND checkout_loaded_dummy = 0
+    
 
 GROUP BY 1,2
